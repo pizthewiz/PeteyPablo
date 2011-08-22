934edeb2b8e6be3fee1b75985a85e2f26f196f6b
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-08-21 22:57:19 -0700

    initial import

---
+++ PeteyPablo/PeteyPabloPlugin.h
@@ -0,0 +1,12 @@
+//
+//  PeteyPabloPlugin.h
+//  PeteyPablo
+//
+//  Created by Jean-Pierre Mouilleseaux on 18 Aug 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface PeteyPabloPlugIn : QCPlugIn
+@end
